229d282e1a36bb1b71ad966d80cb5ad7afe44663
Peter Tsrunchev <tsrunchev@outlook.com>
2018-12-02 11:19:34 +0100

    audio with vector+class input

--- audio.py
+++ audio.py
@@ -1,15 +1,66 @@
 from pydub import AudioSegment
-from pydub.playback import play
 import os
 import numpy as np
+import random
+import subprocess
+from tempfile import NamedTemporaryFile
+import string
 
-f='wav\\Hackathon\\Background'
-files = np.array([f+'\\' + i for i in os.listdir(f) if i[-3:]=='mp3' or i[-3:]='wav'])
-mix = [1,2]
+tempfiles = []
+processes = []
 
-sounds = np.array([AudioSegment.from_file(i, format=i[-3:]) for i in files[mix]])
+def _play_with_ffplay(seg,f):
+    seg.export(f, "wav")
+    return subprocess.Popen(['ffplay', "-nodisp", "-autoexit", "-hide_banner", f])
 
-t = sounds[0].overlay(sounds[1])
-for s in sounds[1:]:
-    t = t.overlay(s)
-play(t)
+def giverd():
+    t = [random.gauss(0.5,0.5) for i in range(1000)]
+    s= sum(t)
+    return [i/s for i in t]
+
+def givewavs(folder):
+    return [os.path.join(dp, f) for dp, dn, fn in os.walk(folder) for f in 
+                                        fn if 
+                                        # f[-3:]=='mp3' or 
+                                        f[-3:]=='wav']
+
+BEAT_FOLDERS=[r'wav\Adrien Fertier_Lo-fi Rock\Loop',]
+        # r'wav\Hackathon\Background']
+SINGLE_FOLDERS = [r'wav\Hackathon\Single', r'wav\Adrien Fertier_Lo-fi Rock\Single Hit']
+SINGLE_NUM = 4
+
+beats = []
+for folder in BEAT_FOLDERS:
+    beats = beats + givewavs(folder)
+beats = np.array(beats)
+
+singles = []
+for folder in SINGLE_FOLDERS:
+    singles = singles + givewavs(folder)
+singles = np.array(singles)
+
+print(str(len(beats)) + 'beats')
+print(str(len(singles)) + 'singles')
+
+# USE THIS - vector, class
+def mix(arr,cl):
+    beat = AudioSegment.from_file(beats[cl])
+    single_i = np.argpartition(singles, -SINGLE_NUM)[-SINGLE_NUM:]
+    single_as = [AudioSegment.from_file(i) for i in singles[single_i[:-1]]]
+
+    t = beat
+    for i,s in enumerate(single_as):
+        times = beat.duration_seconds/2.5
+        t = t.overlay(s[:2500]*int(times),position=i*500)
+    n = 'temp\\' + ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
+    t.export(n,format='wav')
+    return n
+
+if __name__ == "__main__":
+    n = mix(giverd(),random.randint(0,19))
+    input('Play?')
+    p =subprocess.Popen(['ffplay', "-nodisp", "-autoexit", "-hide_banner", n])
+    p.wait()
+
+# files = np.array([f+'\\' + i for i in os.listdir(f) if i[-3:]=='mp3' or i[-3:]=='wav'])
+# mix = [1,5,17]
