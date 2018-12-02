60bd1986198953419cbcc352bef9f5c9529035e8
Alexander Koch <alexander@4koch.de>
2018-12-02 12:08:39 +0100

    Add frequency to each vector element

--- audio.py
+++ audio.py
@@ -1,15 +1,16 @@
-from pydub import AudioSegment
-from pydub.playback import play
+
 import os
 import numpy as np
+import pysine
 
-f='wav\\Hackathon\\Background'
-files = np.array([f+'\\' + i for i in os.listdir(f) if i[-3:]=='mp3' or i[-3:]='wav'])
-mix = [1,2]
+def play_feature_vector(vector):
+    print("feature vector shape")
+    print(vector.shape)
+    vector = vector.flatten()
+    print(vector.shape)
+    for element in range(2):
+        if element < len(vector):
+            print("frequency: " + str(vector[element] * 1000))
+            pysine.add_frequency(vector[element] * 1000)
 
-sounds = np.array([AudioSegment.from_file(i, format=i[-3:]) for i in files[mix]])
-
-t = sounds[0].overlay(sounds[1])
-for s in sounds[1:]:
-    t = t.overlay(s)
-play(t)
+    pysine.play()
